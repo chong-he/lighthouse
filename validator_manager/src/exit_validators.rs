a608cfffe8a905dc488a37347ab3fc8b07c71ee4
Tan Chee Keong <tanck@sigmaprime.io>
2024-10-07 11:11:50 +0800

    single validator

--- validator_manager/src/exit_validators.rs
+++ validator_manager/src/exit_validators.rs
@@ -1,4 +1,4 @@
-use crate::DumpConfig;
+use crate::{common::vc_http_client, DumpConfig};
 use clap::{Arg, ArgAction, ArgMatches, Command};
 use clap_utils::FLAG_HEADER;
 use eth2::SensitiveUrl;
@@ -99,4 +99,22 @@ pub async fn cli_run(matches: &ArgMatches, dump_config: DumpConfig) -> Result<()
     }
 }
 
-async fn run()
+async fn run(config: ExitConfig) -> Result<(), String> {
+    let ExitConfig {
+        vc_url,
+        vc_token_path,
+        validators_to_exit: _,
+    } = config;
+
+    let (http_client, validators) = vc_http_client(vc_url.clone(), &vc_token_path).await?;
+
+    // let exit_epoch: Option<Epoch> = 3000;
+    // let exit_epoch: Option<Epoch>;
+
+    for validator in &validators {
+        let _signing_message =
+            http_client.post_validator_voluntary_exit(&validator.validating_pubkey, None);
+    }
+
+    Ok(())
+}
